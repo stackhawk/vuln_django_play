d6d2ec9dd06c5308a99ee02d6227abc8f14cbbd8
Adam Baldwin <adam.baldwin@stackhawk.com>
2020-06-18 13:54:13 -0600

    Port listening handler

--- app/start-server.sh
+++ app/start-server.sh
@@ -3,5 +3,5 @@
 
 sed -i'' -e "s/%SERVER_PORT%/${SERVER_PORT}/g" /etc/nginx/sites-available/default
 
-(cd /opt/app/vuln_django; gunicorn vuln_django.wsgi  --user www-data --bind 0.0.0.0:8010 --workers 3) &
+(cd /opt/app/vuln_django; gunicorn vuln_django.wsgi  --user www-data --bind 0.0.0.0:${SERVER_PORT} --workers 3) &
 nginx -g "daemon off;"
