6dcde577a994fe9ba9257814e9cfd282dbaba2b1
Adam Baldwin <adam.baldwin@stackhawk.com>
2020-06-18 11:50:24 -0600

    Docker enhancements and docker-compose
    - Flattening the docker build and simplifying steps
    using docker compose to work well with dev build
    process.
    - Add configurable port for the service

--- app/start-server.sh
+++ app/start-server.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # start-server.sh
 
+sed -i'' -e "s/%SERVER_PORT%/${SERVER_PORT}/g" /etc/nginx/sites-available/default
+
 (cd /opt/app/vuln_django; gunicorn vuln_django.wsgi  --user www-data --bind 0.0.0.0:8010 --workers 3) &
 nginx -g "daemon off;"
